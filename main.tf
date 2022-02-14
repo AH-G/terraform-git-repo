90c33672ebe0ef83eccedf782cdfffa834bb6075
Abdul Hannan Ghafoor <ahannan.ghafoor@gmail.com>
2022-02-14 09:41:33 +0500

    adding the prod instance terraform file to the git repo

---
+++ main.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "aws-ami" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "prod_instance" {
+  ami = data.aws_ami.aws-ami.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = "prod_instance"
+  }
+}
